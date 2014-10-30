3d2b0502fa9b6bea736984e4aa8a86ac8073ff19
Donna Murphy <donna.peplinskie@risevision.com>
2014-10-30 16:27:26 -0400

    Updated Widget e2e tests
    Node needs to be cloned every time

--- test/e2e/widget-scenarios.js
+++ test/e2e/widget-scenarios.js
@@ -7,22 +7,21 @@ casper.test.begin("Google Calendar Widget - e2e Testing", function (test) {
   casper.start("http://localhost:"+e2ePort+"/src/widget-e2e.html",
     function () {
       test.assertTitle("Google Calendar Widget", "Test page has loaded");
-      test.assertElementCount(".day", 1, "Check number of days");
-      test.assertElementCount(".event", 1, "Check number of events");
     }
   );
 
-  //casper.waitFor(
-    //function check() {
-      //return this.evaluate(function() {
-        // TODO: Why doesn't this work?
-        //return document.querySelectorAll("li.day").length > 1;
-      //});
-    //},
-    //function then() {
-      //test.assertSelectorHasText(".date", "Today");
-    //}
-  //);
+  casper.waitFor(
+    function check() {
+      return this.evaluate(function() {
+        return document.querySelectorAll(".day").length === 1;
+      });
+    },
+    function then() {
+      test.assertExists(".day");
+      test.assertElementCount(".day", 1, "Check that there are only events for one day");
+      test.assertElementCount(".event", 1, "Check that there is only one event");
+    }
+  );
 
   casper.run(function() {
     test.done();
